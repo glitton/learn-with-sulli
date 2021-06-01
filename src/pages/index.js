226b8680271d940649498e07aa401949fecb6c04
Generosa Litton <glcodeworks@gmail.com>
2021-06-01 13:57:39 -0500

    finished gatsby tutorial part 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,12 @@
-// Step 1: Import React
 import * as React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/layout";
 
-// Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    </Layout>
   );
 };
 
-// Step 3: Export your component
 export default IndexPage;
