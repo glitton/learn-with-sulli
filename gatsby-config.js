3d382517df868e9c1eab41c0218ae82a1656c4ab
Generosa Litton <glcodeworks@gmail.com>
2021-06-09 12:29:38 -0500

    added cute pitbull pics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: "LearnWithSulli",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src="https://soyunperro.com/wp-content/uploads/2019/03/pitbulls.jpg"
+        alt="Pitbull puppies"
+      />
+      <StaticImage
+        src="../images/BennieHandsome.jpg"
+        alt="Bennie the pittbull"
+      />
     </Layout>
   );
 };
