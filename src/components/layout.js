8bd5fbe012d7b89fccd41e6bef0e06b2cf8a535f
Generosa Litton <glcodeworks@gmail.com>
2021-06-30 13:12:45 -0500

    added blog page and queries

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,31 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
   navLinkText,
+  siteTitle,
 } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <main className={container}>
-      <title>{pageTitle}</title>
+      <title>
+        {pageTitle} | {data.site.siteMetadata.title}
+      </title>
+      <p className={siteTitle}>{data.site.siteMetadata.title}</p>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
@@ -22,6 +36,11 @@ const Layout = ({ pageTitle, children }) => {
           <li className={navLinkItem}>
             <Link to="/about" className={navLinkText}>
               About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
             </Link>
           </li>
         </ul>
---
+++ src/pages/blog.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Layout from "../components/layout";
+import { graphql } from "gatsby";
+
+const Blog = ({ data }) => {
+  console.log(data);
+  return (
+    <Layout pageTitle="Blog">
+      <ul>
+        {data.allFile.nodes.map((node) => {
+          return <li key={node.name}>{node.name}</li>;
+        })}
+      </ul>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+      nodes {
+        name
+      }
+    }
+  }
+`;
+export default Blog;
