fc571cc5088262db5d416ba4c1b963b01a0ddb93
Kirsten Lambertsen <kirstenlambertsen@MacBook-Pro.fios-router.home>
2021-07-20 11:46:40 -0400

    complete tutorial part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,28 +1,35 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const Blog = ({ data }) => {
   console.log(data);
   return (
     <Layout pageTitle="Blog">
-      <ul>
-        {data.allFile.nodes.map((node) => {
-          return <li key={node.id}>{node.name}</li>;
+        {data.allMdx.nodes.map((node) => {
+          return <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>{node.frontmatter.date}</p>
+            <MDXRenderer>{node.body}</MDXRenderer>
+            </article>;
         })}
-      </ul>
     </Layout>
   );
 };
 
 export const query = graphql`
-  query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
-      nodes {
-        name
-        id
+query {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        date(formatString: "dddd, MMMM Do YYYY")
+        title
       }
+      id
+      body
     }
   }
+}
 `;
 export default Blog;
