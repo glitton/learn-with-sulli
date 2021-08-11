0d0fc99568618dde5c8422677b9646d3520f56b1
Generosa Litton <glcodeworks@gmail.com>
2021-08-11 13:14:34 -0500

    gatsby image tutorial final

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,13 +1,22 @@
 import * as React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
 
 const BlogPostPage = ({ data }) => {
-  console.log(data);
+  const image = getImage(data.mdx.frontmatter.hero_image);
+  console.log(image);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link} target="_blank">
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
@@ -19,6 +28,17 @@ export const query = graphql`
       frontmatter {
         date
         title
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image_alt
+        hero_image {
+          childImageSharp {
+            gatsbyImageData(
+              aspectRatio: 1.78
+              transformOptions: { cropFocus: CENTER }
+            )
+          }
+        }
       }
       body
     }
